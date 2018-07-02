f232b3b7dc38788ba5431a80c1495af985f903a5
armand <armand.foucault@telecom-bretagne.eu>
2018-07-03 01:36:32 +0200

    Created Application class

--- lib/pyx.py
+++ lib/pyx.py
@@ -8,13 +8,11 @@ class Task:
 
 
 class Config:
-    def __init__(self):
-        pass
-
     @classmethod
-    def get(cls):
-        config_path = os.path.join(os.getcwd(), "pyx_config.py")
+    def get(cls, name):
+        config_path = os.path.join(os.getcwd(), "config", name + ".py")
 
+        print(config_path)
         config_spec = importlib.util.spec_from_file_location("pyx_config", config_path)
         config_module = importlib.util.module_from_spec(config_spec)
         config_spec.loader.exec_module(config_module)
@@ -24,3 +22,25 @@ class Config:
                 return attr
 
         raise Exception("No config found")
+
+
+class Application:
+    def __init__(self, config):
+        self.config = config
+
+    def run(self):
+        raise NotImplementedError
+
+    @classmethod
+    def get(cls):
+        app_path = os.path.join(os.getcwd(), "pyx_app.py")
+
+        app_spec = importlib.util.spec_from_file_location("pyx_app", app_path)
+        app_module = importlib.util.module_from_spec(app_spec)
+        app_spec.loader.exec_module(app_module)
+
+        for attr in app_module.__dict__.values():
+            if isinstance(attr, type) and issubclass(attr, cls):
+                return attr
+
+        raise Exception("No app found")
