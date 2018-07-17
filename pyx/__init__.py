2b85464bb078ad4af792a239941fc50a7107aff1
armand <armand.foucault@telecom-bretagne.eu>
2018-07-18 01:10:01 +0200

    Removed useless app, config and task; will get back later

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -1,11 +1,3 @@
 from importlib import import_module
 
-_application = import_module(".application", __name__)
-_config = import_module(".config", __name__)
-_task = import_module(".task", __name__)
 utils = import_module(".utils", __name__)
-
-Application = _application.Application
-Config = _config.Config
-Task = _task.Task
-TaskFailure = _task.TaskFailure
