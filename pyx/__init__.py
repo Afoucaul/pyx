38694139a81dd4d057fe8174cff48c9d5705e3a7
armand <armand.foucault@telecom-bretagne.eu>
2018-07-19 01:46:26 +0200

    Added main package name to __init__.py

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -1,5 +1,7 @@
 from importlib import import_module
 
+name = "pyx"
+
 utils = import_module(".utils", __name__)
 errors = import_module(".errors", __name__)
 application = import_module(".application", __name__)
