fe4bca222cbcb05616c03551faee204166a9df11
armand <armand.foucault@telecom-bretagne.eu>
2018-07-24 00:59:16 +0200

    Upgraded to 1.6.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyx-manager",
-    version="1.6.2",
+    version="1.6.3",
     entry_points={
         'console_scripts': ["pyx = pyx.__main__:main"]
     },
