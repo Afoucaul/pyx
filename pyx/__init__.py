de18d05fcbb2af135c026712fe067bae200ab0d5
armand <armand.foucault@telecom-bretagne.eu>
2018-07-24 00:44:19 +0200

    Upgraded to v1.6.2

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -1,7 +1,7 @@
 from importlib import import_module
 
 name = "pyx"
-__version__ = "1.6"
+__version__ = "1.6.2"
 
 utils = import_module(".utils", __name__)
 errors = import_module(".errors", __name__)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyx-manager",
-    version="1.6",
+    version="1.6.2",
     entry_points={
         'console_scripts': ["pyx = pyx.__main__:main"]
     },
