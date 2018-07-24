5a912c9beef9a90c8c25e536511757d055704dd0
armand <armand.foucault@telecom-bretagne.eu>
2018-07-24 20:38:53 +0200

    Upgraded to version 1.6.4

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -1,7 +1,7 @@
 from importlib import import_module
 
 name = "pyx"
-__version__ = "1.6.3"
+__version__ = "1.6.4"
 
 utils = import_module(".utils", __name__)
 errors = import_module(".errors", __name__)
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="pyx-manager",
-    version="1.6.3",
+    version="1.6.4",
     entry_points={
         'console_scripts': ["pyx = pyx.__main__:main"]
     },
     packages=setuptools.find_packages(),
     install_requires=[
         'docopt',
-        'colorama'
+        'colorama',
+        'twine'
         ],
     author='Armand Foucault',
     author_email='armand.foucault@telecom-bretagne.eu',
