0ef047c2975475710ddf7611304a30140de8ac73
armand <armand.foucault@telecom-bretagne.eu>
2018-09-13 22:13:40 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("PYPI_README.md", 'r') as f:
 
 setuptools.setup(
     name="pyx-manager",
-    version="1.6.9",
+    version="1.7",
     entry_points={
         'console_scripts': ["pyx = pyx.__main__:main"]
     },
